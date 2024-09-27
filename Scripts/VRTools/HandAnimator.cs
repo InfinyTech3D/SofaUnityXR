4fdbcdfb0574d9fe30c9779cab4d1e58f6d49538
epernod <erik.pernod@gmail.com>
2024-09-27 21:56:20 +0200

    Fix compilation if InputSystem is not there

--- Scripts/VRTools/HandAnimator.cs
+++ Scripts/VRTools/HandAnimator.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+#if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
+#endif
 
 namespace SofaUnityXR
 {
     public class HandAnimator : MonoBehaviour
     {
+#if ENABLE_INPUT_SYSTEM
         [SerializeField] private InputActionProperty m_triggerAnimationAction;
         [SerializeField] private InputActionProperty m_gripAnimationAction;
+#endif
         [SerializeField] private Animator m_handAnimator;
 
         private float m_triggerValue;
@@ -23,11 +28,13 @@ namespace SofaUnityXR
         // Update is called once per frame
         void Update()
         {
+#if ENABLE_INPUT_SYSTEM
             m_triggerValue = m_triggerAnimationAction.action.ReadValue<float>();
             m_handAnimator.SetFloat("Trigger", m_triggerValue);
 
             m_gripValue = m_gripAnimationAction.action.ReadValue<float>();
             m_handAnimator.SetFloat("Grip", m_gripValue);
+#endif
         }
     }
 }
