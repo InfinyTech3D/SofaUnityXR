bafa6c9c1ac65f6a63caa652929762074a03f2b7
PierreFonda3D <pierre.fonda@infinytech3d.com>
2025-01-04 10:16:00 +0100

    Add animation between switch modes,model selction and sphere creation

--- Scripts/Root/SofaModelElementExplorer .cs
+++ Scripts/Root/SofaModelElementExplorer .cs
@@ -15,11 +15,19 @@ namespace SofaUnityXR
         //**************************************//
 
         /// Pointer to the Component Model gameobject related to this button
-        [SerializeField] private GameObject m_targetElement;
+        [SerializeField] public GameObject m_targetElement;
+        [SerializeField] public GameObject m_SofaContextObj;
         [SerializeField] private Toggle m_toggleButton;
         [SerializeField] private Button m_pushButton;
 
-        /// Bool to store the information if this model/button is selected
+        public Vector3 m_simuPosition;
+        public Vector3 m_plannifPosition;
+        public Quaternion m_simuRotation;
+        public Quaternion m_plannifRotation;
+
+        [SerializeField] private SofaModelExplorer m_modelExplorer = null;
+
+         /// Bool to store the information if this model/button is selected
         protected bool isSelected = false;
         [SerializeField] protected Material m_selectedMaterial = null;
         protected Material m_defaultMaterial;
@@ -47,37 +55,37 @@ namespace SofaUnityXR
                 transform.GetComponentInChildren<TextMeshProUGUI>().text = m_targetElement.name;
 
                 m_defaultMaterial = m_targetElement.GetComponent<Renderer>().material;
-
-                if (m_targetElement.GetComponent<BoxCollider>() == null)
-                    m_targetElement.AddComponent<BoxCollider>();
-
-                if (m_targetElement.GetComponent<Rigidbody>() == null)
-                {
-                    m_targetElement.AddComponent<Rigidbody>();
-                    m_targetElement.GetComponent<Rigidbody>().useGravity = false;
-
-                    if (m_targetElement.GetComponent<XRGrabInteractable>() == null)
-                    {
-                        m_targetElement.AddComponent<XRGrabInteractable>();
-
-                        m_targetElement.GetComponent<XRBaseInteractable>().interactionLayers = InteractionLayerMask.GetMask("Default");
-                        m_targetElement.GetComponent<XRGrabInteractable>().throwOnDetach = false;
-                        m_targetElement.GetComponent<XRGrabInteractable>().useDynamicAttach = true;
-                    }
-                    m_targetElement.GetComponent<Rigidbody>().isKinematic = true;
-                }
-
                 m_targetElement.layer = LayerMask.NameToLayer("Grabbable");
+
+                m_simuPosition = m_targetElement.transform.position;
+                m_plannifPosition = m_targetElement.transform.position;
+                m_simuRotation = m_targetElement.transform.rotation;
+                m_plannifRotation = m_targetElement.transform.rotation;
 
                 EventTrigger trigger = m_targetElement.GetComponent<EventTrigger>();
                 if (trigger == null)
                 {
+                    
                     trigger = m_targetElement.AddComponent<EventTrigger>();
                     EventTrigger.Entry entry = new EventTrigger.Entry();
                     entry.eventID = EventTriggerType.PointerClick;
                     entry.callback.AddListener((data) => { OnPointerDownDelegate((PointerEventData)data); });
                     trigger.triggers.Add(entry);
                 }
+                DetermineGrabbableElement(false);
+
+            }
+        }
+
+        public void SetButton()
+        {
+            if(m_pushButton != null)
+            {
+                m_pushButton.GetComponent<Button>().onClick.AddListener(OnButtonClicked);
+            }
+            else
+            {
+                Debug.LogError("Can't find pushButton on the prefab");
             }
         }
 
@@ -86,6 +94,7 @@ namespace SofaUnityXR
         /// </summary>
         public void OnPointerDownDelegate(PointerEventData data)
         {
+
             OnButtonClicked();
         }
 
@@ -115,8 +124,16 @@ namespace SofaUnityXR
         /// assign a child to a button 
         /// </summary>
         public void OnButtonClicked()
-        {
-            bool value = !isSelected; // Toggle selection state
+        {  
+            bool value = true;
+            if (isSelected) // already selected, will unselect
+                value = false;
+
+            if (value)
+                m_modelExplorer.UpdateElementSelected(this);
+            else
+                m_modelExplorer.UpdateElementSelected(null);
+
             OnModelSelectedImpl(value);
         }
 
@@ -142,6 +159,7 @@ namespace SofaUnityXR
         public float GetModelTransparency()
         {
             Material mat = m_targetElement.GetComponent<Renderer>().material;
+            if (mat == null) { Debug.LogError("can't find material"); }
             return mat.GetFloat("_Transparency");
         }
 
@@ -151,6 +169,7 @@ namespace SofaUnityXR
         public void SetModelTransparency(float value)
         {
             Material mat = m_targetElement.GetComponent<Renderer>().material;
+            if(mat == null) { Debug.LogError("can't find material"); }
             mat.SetFloat("_Transparency", value);
 
             if (m_toggleButton != null)
@@ -181,10 +200,14 @@ namespace SofaUnityXR
             OnModelSelectedImpl(false);
         }
 
-        public bool IsTargetActive()
+        public bool IstargetActive()
         {
-            return m_targetElement != null && m_targetElement.activeSelf;
+            if (m_targetElement != null)
+                return m_targetElement.activeSelf;
+
+            return false;
         }
+
 
         //**************************************//
         //***********  Internal API  ***********//
@@ -196,11 +219,40 @@ namespace SofaUnityXR
         protected void OnToggleCheckedImpl(bool value)
         {
             if (m_targetElement != null)
-            {
-                m_targetElement.SetActive(value);
+                m_targetElement.SetActive(m_toggleButton.isOn);
 
+            if (m_targetElement.activeSelf)
+            {
                 Material mat = m_targetElement.GetComponent<Renderer>().material;
-                mat.SetFloat("_Transparency", value ? m_transBeforeHide : 0.0f);
+                mat.SetFloat("_Transparency", m_transBeforeHide);
+                m_modelExplorer.OnUpdateSliderNoNotif();
+            }
+            else
+            {
+                Material mat = m_targetElement.GetComponent<Renderer>().material;
+                m_transBeforeHide = mat.GetFloat("_Transparency");
+                mat.SetFloat("_Transparency", 0.0f);
+                m_modelExplorer.OnUpdateSliderNoNotif();
+            }
+
+        }
+
+        /// <summary>
+        /// change the interaction layer mask og a grabble go to block or not the possibility to grab it;
+        /// parent or child. neither both 
+        /// </summary>
+        /// <param name="value"></param>
+        private void DetermineGrabbableElement(bool value)
+        {
+            if (value)
+            {
+                m_SofaContextObj.GetComponent<XRBaseInteractable>().interactionLayers = InteractionLayerMask.GetMask("Default");
+                m_targetElement.GetComponent<XRBaseInteractable>().interactionLayers = InteractionLayerMask.GetMask(InteractionLayerMask.LayerToName(2));//our object
+            }
+            else
+            {
+                m_SofaContextObj.GetComponent<XRBaseInteractable>().interactionLayers = InteractionLayerMask.GetMask(InteractionLayerMask.LayerToName(2));
+                m_targetElement.GetComponent<XRBaseInteractable>().interactionLayers = InteractionLayerMask.GetMask("Default");
             }
         }
 
@@ -214,21 +266,33 @@ namespace SofaUnityXR
             m_pushButton.image.color = isSelected ? m_pushButton.colors.pressedColor : m_pushButton.colors.normalColor + Color.white;
 
             ResetMaterialFromSelected(value);
+            DetermineGrabbableElement(value);
         }
 
         /// <summary>
         /// update material of the targeted child
         /// </summary>
+        /// <param name="isSelected"></param>
         public void ResetMaterialFromSelected(bool isSelected)
         {
+            string target = m_targetElement.name;
             if (isSelected)
             {
+                float transparency = m_modelExplorer.GetTransparencyByName(target);
                 m_targetElement.GetComponent<Renderer>().material = m_selectedMaterial;
+                m_modelExplorer.SetTransparencyByName(transparency, target);
             }
             else
             {
+                float transparency = m_modelExplorer.GetTransparencyByName(target);
                 m_targetElement.GetComponent<Renderer>().material = m_defaultMaterial;
+                m_modelExplorer.SetTransparencyByName(transparency, target);
             }
+
+        }
+        public void SetModelExplorer(SofaModelExplorer modelExplorer)
+        {
+            m_modelExplorer = modelExplorer;
         }
     }
 }
--- Scripts/Root/SofaModelExplorer.cs
+++ Scripts/Root/SofaModelExplorer.cs
@@ -17,8 +17,10 @@ namespace SofaUnityXR
         [SerializeField] private GameObject m_componentScroller = null;
         [SerializeField] private GameObject TogglePrefab;
 
-        protected List<SofaModelElementExplorer> m_modelElementCtrls = null;
-        protected SofaModelElementExplorer m_targetElement = null;
+        public List<SofaModelElementExplorer> m_modelElementCtrls = null;
+
+        //selected element
+        public SofaModelElementExplorer m_targetElement = null;
 
         [SerializeField] private Slider m_sliderSelected;
         [SerializeField] private Slider m_sliderOthers;
@@ -38,7 +40,7 @@ namespace SofaUnityXR
         private bool m_modelCreationLaunched = false;
         private bool m_modelEnabled = false;
 
-        private string m_modelName;
+        public string m_modelName;
 
         void Awake()
         {
@@ -58,13 +60,34 @@ namespace SofaUnityXR
             {
                 foreach (GameObject obj in m_SofaMeshs)
                 {
+                    //Set the good shader
+                    Renderer renderer = obj.GetComponent<Renderer>();
+                    if (renderer != null && renderer.material != null)
+                    {
+                        Shader newShader = Shader.Find("Custom/Diffuse_Stipple_Transparency");
+                        if (newShader != null)
+                        {                        
+                            renderer.material.shader = newShader;
+                        }
+                        else
+                        {
+                            Debug.LogError("Shader 'Custom/Diffuse_Stipple_Transparency' not found!");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("Renderer or Material is missing on sofavisualmodels");
+                    }
+                    //set ui btn 
                     var btn = Instantiate(TogglePrefab).GetComponent<SofaModelElementExplorer>();
+                    btn.SetModelExplorer(this);
                     btn.transform.SetParent(m_componentScroller.transform);
                     btn.transform.localScale = TogglePrefab.transform.localScale;
                     btn.transform.localPosition = Vector3.zero;
                     btn.transform.localRotation = Quaternion.identity;
                     btn.TargetElement = obj;
                     btn.name = obj.name;
+                    btn.SetButton();
                     m_modelElementCtrls.Add(btn);
                 }
             }
@@ -90,9 +113,48 @@ namespace SofaUnityXR
 
         void Update()
         {
-            // Any update logic can go here
+            /*if (m_modelController.GetThreadLoader() != null && !m_modelController.GetThreadLoader().AllLoaded)
+            {
+                m_modelController.LoadModel();
+            }
+            if (m_modelController.GetThreadLoader() != null && m_modelController.GetThreadLoader().AllLoaded && !m_modelController.NewGameObject)
+            {
+                if (m_modelController.GetTargetModel())
+                    m_modelController.UpdateBoxCollider(m_modelController.GetTargetModel());
+                if (!m_modelController.NextFrame)
+                {
+                    m_modelController.InitiateModelPosition(m_modelController.GetTargetModel());
+                    m_modelController.ModelLoaded = true;
+                    m_modelController.NextFrame = true;
+                }
+            }
+
+            if (m_modelController.GetThreadLoader() != null && m_modelController.GetThreadLoader().AllLoaded && m_modelController.NewGameObject)
+            {
+                m_modelController.GetChildrenWithMeshFilter();
+                m_modelController.DeleteChildrenWithoutMeshFilter();
+                if (!m_modelController.NextFrame)
+                    m_modelController.NextFrame = true;
+                else
+                {
+                    m_modelController.SetTargetModel(m_modelController.GetModelParent());
+
+                    ResetMaterial();
+                    ResetTransparancy();
+                    PopulateModelElements();
+
+                    m_modelController.NextFrame = false;
+                    m_modelController.NewGameObject = false;
+                }
+
+            }*/
+
         }
 
+        /// <summary>
+        /// select the targeted child of a the model when clicking on a button
+        /// </summary>
+        /// <param name="elem"></param>
         public void UpdateElementSelected(SofaModelElementExplorer elem)
         {
             if (m_targetElement)
@@ -102,6 +164,144 @@ namespace SofaUnityXR
 
             if (m_sliderSelected != null && m_targetElement != null)
                 m_sliderSelected.value = m_targetElement.GetModelTransparency();
+        }
+
+
+        /// <summary>
+        /// set the slider value for a given child when clickin on a button 
+        /// </summary>
+        /// <param name="elementName"></param>
+        /// <param name="value"></param>
+        public void EmulateElementSelected(string elementName, bool value)
+        {
+            if (m_targetElement && m_targetElement.name == elementName) // nothing to do
+                return;
+
+            foreach (var item in m_modelElementCtrls)
+            {
+                if (item.name == elementName)
+                {
+                    m_targetElement = item;
+                    m_targetElement.EmulateButtonClicked(value);
+
+                    if (m_sliderSelected != null)
+                        m_sliderSelected.value = m_targetElement.GetModelTransparency();
+                }
+                else
+                    item.EmulateButtonClicked(false);
+            }
+        }
+
+        /// <summary>
+        /// update the state of the toggle
+        /// </summary>
+        /// <param name="elementName"></param>
+        /// <param name="value"></param>
+        public void EmulateElementToggle(string elementName, bool value)
+        {
+            foreach (var item in m_modelElementCtrls)
+            {
+                if (item.name == elementName)
+                {
+                    item.EmulateToggleChecked(value);
+                    break;
+                }
+            }
+        }
+
+
+        /// Method called from user interaction click on slide, real work is done in @sa OnSliderChangedImpl
+        public void OnSliderChanged(Slider ctrl)
+        {
+
+            if (m_targetElement)
+                OnSliderChangedImpl(ctrl);
+        }
+
+
+        public void OnSliderChanged(Slider ctrl, float value)
+        {
+            if (m_targetElement == null)
+                return;
+
+            ctrl.value = value;
+            OnSliderChangedImpl(ctrl);
+        }
+
+        /// <summary>
+        /// update the slider value 
+        /// </summary>
+        /// <param name="sliderName"></param>
+        /// <param name="value"></param>
+        public void EmulateSliderChanged(string sliderName, float value)
+        {
+            if (value > 1) // cast problem from oculus
+            {
+                EmulateSliderChanged(sliderName, value * 0.1f);
+                return;
+            }
+
+            if (m_sliderSelected.name == sliderName)
+            {
+                m_sliderSelected.value = value;
+                OnSliderChangedImpl(m_sliderSelected);
+            }
+            else if (m_sliderOthers.name == sliderName)
+            {
+                m_sliderOthers.value = value;
+                OnSliderChangedImpl(m_sliderOthers);
+            }
+        }
+
+
+        /// <summary>
+        /// hide model on button "hide (x)" clicked
+        /// </summary>
+        public void OnHideClicked()
+        {
+            if (m_targetElement)
+            {
+                bool value = !m_targetElement.IstargetActive();
+                OnHideClickedImpl(value);
+            }
+        }
+
+        /// <summary>
+        /// enable/disable helps
+        /// </summary>
+        /// <param name="help"></param>
+        public void OnHelpClicked(GameObject help)
+        {
+            help.SetActive(!help.activeSelf);
+        }
+
+        /// <summary>
+        /// unselect element by calling it
+        /// </summary>
+        public void OnUnselectClicked()
+        {
+            if (m_targetElement)
+            {
+                UpdateElementSelected(null);
+            }
+
+        }
+
+
+
+        /// <summary>
+        /// show all model on button "show all"  clicked
+        /// </summary>
+        public void OnShowAllClicked()
+        {
+            OnShowAllClickedImpl();
+        }
+
+
+        public void OnResetPositionClicked()
+        {
+            //TODO 
+            //need to connect with gameController 
         }
 
         public void ResetMaterial()
@@ -129,6 +329,217 @@ namespace SofaUnityXR
                 }
             }
         }
+
+        /// <summary>
+        /// get the actual transparency of one item by name when changing material
+        /// </summary>
+        /// <returns></returns>
+        public float GetTransparencyByName(string name)
+        {
+            float transparency = -1f;
+            if (m_modelElementCtrls.Count > 0)
+            {
+                foreach (var item in m_modelElementCtrls)
+                {
+                    if (item.name == name)
+                        return item.GetModelTransparency();
+                }
+            }
+            return transparency;
+        }
+
+        /// <summary>
+        /// set transparancy to one item by name when changing material
+        /// </summary>
+        /// <param name="transparencyList"></param>
+        public void SetTransparencyByName(float transparency, string name)
+        {
+            if (m_modelElementCtrls.Count > 0)
+            {
+                foreach (var item in m_modelElementCtrls)
+                {
+                    if (item.name == name)
+                    {
+                        item.SetModelTransparency(transparency);
+                        return;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Isolate model 
+        /// </summary>
+        protected bool isolate = true;
+        public void OnIsolateClicked()
+        {
+            if (m_targetElement)
+            {
+                OnIsolateClickedImpl(isolate);
+                isolate = !isolate;
+            }
+        }
+
+
+        /// <summary>
+        /// update slider
+        /// </summary>
+        public void OnUpdateSliderNoNotif()
+        {
+            if (m_targetElement != null)
+            {
+                m_sliderSelected.SetValueWithoutNotify(m_targetElement.GetModelTransparency());
+            }
+
+            foreach (var item in m_modelElementCtrls)
+            {
+                if (item == m_targetElement)
+                    continue;
+
+                m_sliderOthers.SetValueWithoutNotify(item.GetModelTransparency());
+                break;
+            }
+        }
+
+        /// <summary>
+        /// update slider
+        /// </summary>
+        /// <param name="status"></param>
+        public void ParseComponentStatus(ViewerStatus status)
+        {
+            foreach (var item in m_modelElementCtrls)
+            {
+                status.m_elementTransparency[item.name] = item.GetModelTransparency();
+                status.m_elementActivated[item.name] = item.IstargetActive();
+            }
+
+            if (m_targetElement)
+            {
+                m_sliderSelected.SetValueWithoutNotify(status.m_elementTransparency[m_targetElement.name]);
+            }
+            m_sliderOthers.SetValueWithoutNotify(status.m_sliderOthers);
+        }
+
+        public void SetComponentStatus(ViewerStatus status)
+        {
+            foreach (var item in m_modelElementCtrls)
+            {
+                item.SetModelTransparency(status.m_elementTransparency[item.name]);
+                item.EmulateToggleChecked(status.m_elementActivated[item.name]);
+            }
+            status.m_sliderOthers = m_sliderOthers.value;
+        }
+
+        //**************************************//
+        //***********  Internal API  ***********//
+        //**************************************//
+
+        /// Internal Method to do the work when a slider is changed
+        protected void OnSliderChangedImpl(Slider ctrl)
+        {
+            if (ctrl == m_sliderSelected)
+            {
+                if (m_targetElement)
+                {
+                    m_targetElement.SetModelTransparency(ctrl.value);
+                }
+            }
+            else if (ctrl == m_sliderOthers)
+            {
+                foreach (var item in m_modelElementCtrls)
+                {
+                    if (item == m_targetElement)
+                        continue;
+
+                    item.SetModelTransparency(ctrl.value);
+                }
+            }
+        }
+
+
+        /// Internal Method to Hide selected
+        public void OnHideClickedImpl(bool value)
+        {
+            m_targetElement.EmulateToggleChecked(value);
+        }
+
+        /// <summary>
+        /// show all models
+        /// </summary>
+        public void OnShowAllClickedImpl()
+        {
+            foreach (var item in m_modelElementCtrls)
+            {
+                item.EmulateToggleChecked(true);
+            }
+        }
+
+        /// <summary>
+        /// reset the transparancy and the slider
+        /// </summary>
+        public void OnResetClickedImpl()
+        {
+            OnSliderChanged(m_sliderSelected, 1.0f);
+            OnSliderChanged(m_sliderOthers, 1.0f);
+
+        }
+
+        /// <summary>
+        /// isolate a model
+        /// </summary>
+        /// <param name="isolate"></param>
+        public void OnIsolateClickedImpl(bool isolate)
+        {
+            foreach (var item in m_modelElementCtrls)
+            {
+                if (item == m_targetElement)
+                    continue;
+
+                item.EmulateToggleChecked(!isolate);
+            }
+        }
+
+        /// <summary>
+        /// Quit the app
+        /// </summary>
+        public void OnQuitButtonClicked()
+        {
+            Application.Quit();
+        }
+
+
+        /*public ModelController ModelController
+        {
+            set => m_modelController = value;
+            get => m_modelController;
+        }*/
+
+        public SofaModelElementExplorer TargetElement
+        {
+            get => m_targetElement;
+        }
+
+        public bool ModelCreationLaunched
+        {
+            get => m_modelCreationLaunched;
+            set => m_modelCreationLaunched = value;
+        }
+
+        public bool ModelEnable
+        {
+            get => m_modelEnabled;
+            set => m_modelEnabled = value;
+        }
+
+        public string ModelName
+        {
+            get => m_modelName;
+        }
+
+
+
+        //adds for sofa version//
+
 
         private void FindRenderer()
         {
